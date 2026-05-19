831039c4baa66708fe1d9e611a5791f4503920f4
gildeb <48280441+gildeb@users.noreply.github.com>
2026-05-19 22:53:31 +0200

    Add files via upload

--- robot/mac_addr.py
+++ robot/mac_addr.py
@@ -1,3 +1,5 @@
+import network
+
 robot_mac = {
     1 : b'H\xf6\xeez/\xd4',
     2 : b'H\xf6\xeez0(',
@@ -5,18 +7,33 @@ robot_mac = {
     4 : b'H\xf6\xeez/,',
     5 : b'H\xf6\xeez3\x84',
     6 : b'H\xf6\xeez4&',
-    7 : b''
+    7 : b'H\xf6\xeez/\xa6',
+    8 : b'H\xf6\xeez/\xf6'
     }
 
 telecommande_mac = {
-#     1 : b'x!\x84\x88\xae\xdc',
     1 : b'\x88\x13\xbf\xfc\xe3\xbc',
     2 : b'\x88\x13\xbf\xfc\xe8\xf0',
     3 : b'\x88\x13\xbf\xfc\xe4\xfc',
     4 : b'\x88\x13\xbf\xfc\xe9p',
     5 : b'\x88\x13\xbf\xfc\xce\xe0',
     6 : b'\x88\x13\xbf\xfc\xe5|',
-    7 : b''
+    7 : b'\x88\x13\xbf\xfc\xec4',
+    8 : b'\x88\x13\xbf\xfc\xe5\x80'
     }
 
-baseAddr = b'$X|\x91\xe0\xd8' 
+print("Mon adresse MAC:", network.WLAN().config("mac"))
+
+def find_robot():
+    mac = network.WLAN().config("mac")
+    for i in robot_mac:
+        if mac == robot_mac[i]:
+            print("je suis le robot n°", i)
+            return i, "robot"
+    print("no robot")
+    for i in telecommande_mac:
+        if mac == telecommande_mac[i]:
+            print("je suis la telecommande n°", i)
+            return i, "telecommande"
+    print("no télécommande")
+    return None, "unknown robot"
